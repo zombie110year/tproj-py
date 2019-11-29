99b773c2b8cf14d1c5c796fbe8b26a5cf8130c5a
zombie110year <zombie110year@outlook.com>
2019-11-29 20:41:47 +0800

    :wrench: add setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,38 @@
+from pathlib import Path
+
 from setuptools import setup
 
+from tproj import __version__
+
 setup(
-    
+    author="zombie110year",
+    author_email="zombie110year@outlook.com",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Mircrosoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="管理项目模板",
+    entry_points = {
+        "console_scripts": [
+            "tproj = tproj.cli:main",
+        ],
+    },
+    requires=[
+        "pyyaml"
+    ],
+    license="MIT",
+    long_description=Path("README.md").read_text("utf-8"),
+    long_description_content_type="text/markdown",
+    maintainer="zombie110year",
+    maintainer_email="zombie110year@outlook.com",
+    name="tproj-zombie110year",
+    packages=["tproj", "tproj.utils"],
+    platforms=["win32", "linux"],
+    python_requires='>=3.7',
+    url="https://github.com/zombie110year/tproj-py/",
+    version=__version__,
 )
