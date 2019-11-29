cafd720befc3c64edab733419358006583e87bff
zombie110year <zombie110year@outlook.com>
2019-11-30 00:58:40 +0800

    feat: create template

--- tproj/create.py
+++ tproj/create.py
@@ -8,6 +8,8 @@ from typing import Union
 from typing import List
 
 from .utils import parse_cfg
+from .utils import create_mem_zip
+from .utils import get_template_dir
 
 __all__ = (
     "create_main",
@@ -21,16 +23,28 @@ def create_main(name: Union[str, None], src: str, force: bool):
     :param str src: 要打包的目录
     :param bool force: 是否强制覆写已存在的同名归档
     """
-    src_path = Path(src)
-    cfg_path = Path(src_path / "tproj.yml")
-    if cfg_path.exists():
-        cfg = parse_cfg(cfg_path.read_text())
+    zf_file = Path(get_template_dir() / "{}.zip".format(name))
+    if not force:
+        overwrite = input(
+            "there is a exsiting template named {}, overwrite? [y/n] ").strip().startswith("y")
     else:
-        cfg = parse_cfg(None)
-    if cfg.get("name") == "":
-        name = input("Template Name: ").strip()
+        overwrite = force
 
-    files = read_src_dir(src_path, cfg.get("include"))
+    if overwrite:
+
+        src_path = Path(src)
+        cfg_path = Path(src_path / "tproj.yml")
+        if cfg_path.exists():
+            cfg = parse_cfg(cfg_path.read_text())
+        else:
+            cfg = parse_cfg(None)
+        if cfg.get("name") == "":
+            name = input("Template Name: ").strip()
+
+        files = read_src_dir(src_path, cfg.get("include"))
+        zf_content = create_mem_zip(files)
+        zf_file.write_bytes(zf_content)
+        print("template saved at {}".format(zf_file.as_posix()))
 
 
 def read_src_dir(src: Path, include: List[str]) -> List[Path]:
