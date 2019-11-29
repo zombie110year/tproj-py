7049dc1ad6b84bfbee6b6ae82ad909d7fbd8d5b2
zombie110year <zombie110year@outlook.com>
2019-11-29 21:31:18 +0800

    添加 parse_cfg 用来解析 yml 格式的配置文件

--- tproj/utils/__init__.py
+++ tproj/utils/__init__.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from typing import Union
 from warnings import warn
 
+import yaml
+
 __all__ = (
     "get_tproj_home",
     "get_template_dir",
@@ -52,6 +54,32 @@ def ensure_file_exist(path: Union[str, Path]):
     if not path.exists():
         ensure_dir_exist(path.parent)
         path.touch()
+
+
+def parse_cfg(conf_text: str) -> dict:
+    """解析创建模板归档的配置文件
+
+    |   键    |   类型    |  默认值  | 含义                      |
+    | :-----: | :-------: | :------: | ------------------------- |
+    |  name   |    str    |   `""`   | 此模板的名字              |
+    | author  |    str    |   `""`   | 模板的作者，`name<email>` |
+    | include | List[str] | `["**"]` | 默认包含所有文件、子目录  |
+
+    :param conf_path: 配置文件的路径
+    """
+    default = {
+        "name": "",
+        "author": "",
+        "include": [
+            "**"
+        ]
+    }
+    config = yaml.load(conf_text, Loader=yaml.SafeLoader)
+    assert isinstance(config, dict)
+    for k in default.keys():
+        if k in config.keys():
+            default[k] = config.get(k)
+    return default
 
 
 def get_default_tproj_home() -> Path:
