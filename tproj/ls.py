9721315373f21767149d65447a9807375d5a1edd
zombie110year <zombie110year@outlook.com>
2019-11-29 23:30:28 +0800

    feat: ls

--- tproj/ls.py
+++ tproj/ls.py
@@ -1,3 +1,13 @@
+from pathlib import Path
+from .utils import get_template_dir
+
+from typing import List
+
 def ls_main():
     """列出所有可用的模板归档"""
-    pass
+    for i in get_templates():
+        print("{}".format(i.stem))
+
+def get_templates() -> List[Path]:
+    template_dir = get_template_dir()
+    return list(template_dir.glob("*.zip"))
